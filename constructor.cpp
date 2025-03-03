d664a9fb1f9dface73557b29d16dc8e1003f4ea2
humaaijaz <146563055+humaaijaz@users.noreply.github.com>
2025-03-03 14:53:05 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,16 @@
+/* Program to  show c working of onstructor
+*/
+# include <iostream>
+using namespace std;
+class Program //class name
+{
+    public:Program() //Default Constructor
+    {
+        cout <<" Default Constructor ";
+    }
+};
+int main()
+{
+    Program obj; //class object
+  return 0;
+}
